0bc791e3b2375d47ac192828f61f3d0d09017503
RupanshiDhariwal <68110556+RupanshiDhariwal@users.noreply.github.com>
2023-02-16 18:32:18 +0530

    third commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 // mongodb://localhost:27017/solnstechDB
 
-mongoose.connect("mongodb+srv://hrmsuser:Solns@2022@clustersolnstech.1bikubl.mongodb.net/solnstechDB?retryWrites=true&w=majority", {useNewUrlParser : true});
+mongoose.connect("mongodb+srv://root:root@clustersolnstech.1bikubl.mongodb.net/solnstechDB?retryWrites=true&w=majority", {useNewUrlParser : true});
 const userSchema = {
     email: String,
     password : String
